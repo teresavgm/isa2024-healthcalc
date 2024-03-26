bfb174ac5bfa781928f81cc4e7e47be997ef1864
teresavgm <160473130+teresavgm@users.noreply.github.com>
2024-03-26 16:27:45 +0100

    Implementacion final CucumberStep.java

--- src/test/java/uma/healthcalc/BDD/CucumberStep.java
+++ src/test/java/uma/healthcalc/BDD/CucumberStep.java
@@ -1,81 +1,89 @@
-package test.java.uma.healthcalc.BDD;
+package uma.healthcalc.BDD;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import healthcalc.HealthCalcImpl;
 
-public class CucumberTest {
+public class CucumberStep {
 
     private float weight;
     private int height;
     private char gender;
     private int age;
     private boolean raiseException;
-    private float resultado;
-    private HealthCalcImpl calc= new HealthCalcImpl();
+    private float resultado1;
+    private float resultado2;
 
+    private HealthCalcImpl calcImpl= new HealthCalcImpl();
 
-    @Given("my gender {string} and heigth {int}")
-public void my_gender_and_heigth(String string, Integer int1) {
-    height=int1;
-    gender=string.charAt(0);
+    
+    //BMR
+    @Given("my gender {string}, weight {int}, height {int} and age {int} correctly")
+    public void my_gender_weight_height_and_age_correctly(String s, Integer int1, Integer int2, Integer int3) {
+        weight= int1;
+        height= int2;
+        gender=s.charAt(0);
+        age= int3;
 
-}
-@Given("my gender {string} or heigth {int} incorrectly")
-public void my_gender_or_heigth_incorrectly(String string, Integer int1) {
-    height=int1;
-    gender=string.charAt(0);
-}
-@Then("The system raises an exception")
-public void the_system_raises_an_exception() {
-    assertEquals(true, raises_error);
-}
-
-@When("I calcule my ideal weight")
-public void i_calcule_my_ideal_weight() {
-    try{
-        resultado=calc.idealWeight(height,gender);
-    }catch(Exception e){
-        raises_error=true;
     }
-}
-
-@Then("I should see my ideal wheight {int}")
-public void i_should_see_my_ideal_wheight(float int1) throws Exception {
-    assertEquals(int1,resultado);
-}
-
-@Then("I should see my imb {float}")
-public void i_should_see_my_imb(float int1) throws Exception {
-    assertEquals(int1,resultado);
-}
-
-@When("I calcule my imb")
-public void i_calcule_my_imb() {
-    try{
-        resultado=calc.basalMetabolicRate(weight,height,gender,age);
-    }catch(Exception e){
-        raises_error=true;
+    @When("I calcule my BMR")
+    public void i_calcule_my_BMR() {
+        try{ 
+            resultado1=calcImpl.basalMetabolicRate(weight,height,gender,age);
+        }catch(Exception e){
+            raiseException = true;
+        }
     }
-}
 
-@Given("my gender {string}, heigth {int}, weight {int} and age {int}")
-public void my_gender_heigth_weight_and_age(String string, Integer int1, Integer int2, Integer int3) {
-    weight=int2;
-    height=int1;
-    gender=string.charAt(0);
-    age=int3;
-}
+    @Then("the system return my BMR {float}")
+    public void the_system_return_my_BMR(float int1){
 
-@Given("my gender {string}, heigth {int}, weight {int} or age {int} incorrectly")
-public void my_gender_heigth_weight_or_age_incorrectly(String string, Integer int1, Integer int2, Integer int3) {
-    weight=int2;
-    height=int1;
-    gender=string.charAt(0);
-    age=int3;
+        assertEquals(int1,resultado1);
+    }
+    @Given("at least my gender {string}, weight {int}, height {int} or age {int} incorrectly")
+    public void at_least_my_gender_heigth_weight_or_age_incorrectly(String g, Integer h, Integer w, Integer a) {
+        weight= w;
+        height= h;
+        gender=g.charAt(0);
+        age= a;
+    } 
+    @Then("the system raises an exception")
+    public void the_system_raises_an_exception() {
+        assertEquals(true, raiseException);
+    }
+    
+   
+    //idealWeight
+
+    @Given("my gender {string} and height {int} correctly")
+    public void my_gender_and_height_correctly(String s, Integer int1) {
+        height=int1;
+        gender=s.charAt(0);
+    }
+    @When("I calcule my ideal weight")
+    public void i_calcule_my_ideal_weight() {
+        try{
+            resultado2=calcImpl.idealWeight(height,gender);
+        }catch(Exception e){
+            raiseException=true;
+        }
+    }
+    @Then("the system return my ideal weight {float}")
+    public void the_system_return_my_ideal_weight(float idealw) {
+        assertEquals(idealw,resultado2);
+        
+    }
+    @Given("my height {int} in negative or an unrecognizable gender {string}")
+    public void my_height_in_negative_or_an_unrecognizable_gender(Integer h, String s) {
+        height=h;
+        gender=s.charAt(0);
+    }
+    
+    
+    
 }
-}
+    
+    
