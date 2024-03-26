c35dc077f4f9e0b7bfd2e9df2fb09c27b489dc27
teresavgm <160473130+teresavgm@users.noreply.github.com>
2024-03-26 16:27:14 +0100

    Implementacion final RunCucumberTest.java

--- src/test/java/uma/healthcalc/BDD/RunCucumberTest.java
+++ src/test/java/uma/healthcalc/BDD/RunCucumberTest.java
@@ -1,4 +1,4 @@
-package test.java.uma.healthcalc.BDD;
+package uma.healthcalc.BDD;
 
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
@@ -9,7 +9,9 @@ import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber")
-@SelectClasspathResource("resources")
+@SelectClasspathResource("uma")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
 public class RunCucumberTest {
 }
+
+
