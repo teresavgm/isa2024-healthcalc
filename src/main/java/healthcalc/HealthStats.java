abbfd9617da681c08cab96651f5d750faea19def
teresavgm <160473130+teresavgm@users.noreply.github.com>
2024-05-08 22:27:14 +0200

    Implemento la interfaz HealthStats

---
+++ src/main/java/healthcalc/HealthStats.java
@@ -0,0 +1,14 @@
+package healthcalc;
+
+public interface HealthStats {
+
+
+    public float alturaMedia();
+    public float pesoMedio();
+    public float edadMedia();
+    public float bmrMedio();
+    public int numSexoH();
+    public int  numSexoM();
+    public int numTotalPacientes();
+    
+}
