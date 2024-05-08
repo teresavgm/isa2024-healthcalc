147b8831aad2fd2fc794cc5422a1e0cf65aaae56
teresavgm <160473130+teresavgm@users.noreply.github.com>
2024-05-09 01:51:15 +0200

    Cambio las unidades de los parametros de entrada a gramos y a metros para probar correctamente Adapter

--- src/main/java/healthcalc/AdapterMain.java
+++ src/main/java/healthcalc/AdapterMain.java
@@ -1,21 +1,16 @@
 package healthcalc;
 
-
-import java.awt.EventQueue;
-
-import healthcalc.HealthCalcImpl;
-
 public class AdapterMain {
     public static void main(String[] args) {
         HealthCalc healthCalc = HealthCalcImpl.getInstance();
         HealthHospital adapter = new Adapter(healthCalc);
 
         // Utiliza el adapter para calcular el BMR
-        double bmr = adapter.bmr('m', 30, 175.0f, 70);
+        double bmr = adapter.bmr('m', 50, (float) 1.72, 70000);
         System.out.println("Basal Metabolic Rate (BMR): " + bmr);
 
         // Utiliza el adapter para calcular el peso ideal
-        int pesoIdeal = adapter.pesoIdeal('m', 175.0f);
+        int pesoIdeal = adapter.pesoIdeal('m', (float)1.5);
         System.out.println("Peso Ideal: " + pesoIdeal);
 
 
