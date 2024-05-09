7b6b18173339ff0c04e0b0488009a4003163a2f8
teresavgm <160473130+teresavgm@users.noreply.github.com>
2024-05-09 19:11:46 +0200

    Creo la clase MainProxy para comprobar que se ha implementado correctamente el patrón

---
+++ src/main/java/healthcalc/MainProxy.java
@@ -0,0 +1,28 @@
+package healthcalc;
+
+public class MainProxy {
+
+    public static void main(String[] args) throws Exception {
+ 
+    HealthCalc healthCalc = HealthCalcImpl.getInstance();
+    Adapter calc = new Adapter(healthCalc);
+    HealthStatsProxy nuevaCalc = new HealthStatsProxy(calc);
+
+    //Utilizamos la calculadora para introducir datos
+    double bmr1 = nuevaCalc.bmr('w', 25, (float)1.4, 45000);
+    double bmr2 = nuevaCalc.bmr('m', 50, (float)1.8, 70000);
+
+    System.out.println("BMR Paciente 1: " + bmr1 );
+    System.out.println("BMR Paciente 2: " + bmr2 );
+    System.out.println("    ");
+
+    System.out.println("ALTURA MEDIA EN METROS: " + nuevaCalc.alturaMedia());
+    System.out.println("EDAD MEDIA: " +nuevaCalc.edadMedia());
+    System.out.println("PESO MEDIO EN GRAMOS: " +nuevaCalc.pesoMedio());
+    System.out.println("NÚMERO DE PACIENTES MUJER: " +nuevaCalc.numSexoM());
+    System.out.println("NÚMERO DE PACIENTES HOMBRE: " +nuevaCalc.numSexoH());
+    System.out.println("BMR MEDIO: " + nuevaCalc.bmrMedio());
+
+
+    }
+}
