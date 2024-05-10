d7355fc1417dfbc0386571af3a00a2205ad5350e
teresavgm <160473130+teresavgm@users.noreply.github.com>
2024-05-10 19:18:03 +0200

    Implemento la clase DecoradorAmericano.java

---
+++ src/main/java/healthcalc/DecoradorAmericano.java
@@ -0,0 +1,34 @@
+package healthcalc;
+
+public class DecoradorAmericano extends DecoratorHealthHospital {
+
+    public DecoradorAmericano(HealthHospital h) {
+        super(h);
+        //TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public int pesoIdeal(char genero,float alturaFt) throws Exception {
+        //pasamos la altura introducida en pies a metros
+        float alturaMetros = alturaFt * 0.3048f;
+        float pi = super.pesoIdeal(genero, alturaMetros);
+
+        return (int) pi ;
+    }
+
+    @Override
+    public double bmr(char genero, int edad, float alturaFt, int pesoLb) throws Exception {
+        float alturaMetros = alturaFt * 0.3048f;
+        int pesoGr = (int) (pesoLb * 453.592);
+
+        float BMR = (float) super.bmr(genero, edad,alturaMetros, pesoGr );
+
+        //Imprimimos los resultados por pantalla en inglés y en español
+        System.out.println("The person whose height is " + alturaFt + " ft" + " and whose weight is " + pesoLb + " Lb has a BMR of " + BMR + " calories");
+        System.out.println("La persona con altura " + alturaMetros + " metros " + " y peso de " + pesoGr + " Gr tiene un BMR de " + BMR + " calorias");
+
+        return BMR;
+    }
+
+
+}
