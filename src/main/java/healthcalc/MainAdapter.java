dba12920d9c038a32435fa0b9935ab60b84a0422
teresavgm <160473130+teresavgm@users.noreply.github.com>
2024-05-10 19:25:41 +0200

    Correccion de excepciones en MainAdapter.java

---
+++ src/main/java/healthcalc/MainAdapter.java
@@ -0,0 +1,21 @@
+package healthcalc;
+
+public class MainAdapter {
+    public static void main(String[] args) throws Exception {
+        HealthCalc healthCalc = HealthCalcImpl.getInstance();
+        HealthHospital adapter = new Adapter(healthCalc);
+
+        // Utiliza el adapter para calcular el BMR
+        double bmr = adapter.bmr('m', 50, (float) 1.72, 70000);
+        System.out.println("Basal Metabolic Rate (BMR): " + bmr);
+
+        // Utiliza el adapter para calcular el peso ideal
+        int pesoIdeal = adapter.pesoIdeal('m', (float)1.5);
+        System.out.println("Peso Ideal: " + pesoIdeal);
+
+
+        
+
+    }
+
+}
