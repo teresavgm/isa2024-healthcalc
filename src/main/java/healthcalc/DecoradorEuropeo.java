577a6b422403d426854bde1c0e3106633e896b9c
teresavgm <160473130+teresavgm@users.noreply.github.com>
2024-05-10 19:18:48 +0200

    Implemento la clase DecoradorEuropeo.java

---
+++ src/main/java/healthcalc/DecoradorEuropeo.java
@@ -0,0 +1,29 @@
+package healthcalc;
+
+public class DecoradorEuropeo extends DecoratorHealthHospital {
+
+
+    public DecoradorEuropeo(HealthHospital h) {
+        super(h);
+        //TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public int pesoIdeal(char genero,float altura) throws Exception {
+        return super.pesoIdeal(genero, altura);
+    }
+
+    @Override
+    public double bmr(char genero, int edad, float alturaMetros, int pesoGr) throws Exception {
+        float BMR = (float) super.bmr(genero, edad,alturaMetros, pesoGr );
+        double alturaFt = alturaMetros*3.28084;
+        int pesoLb = (int) (pesoGr * 0.00220462);
+
+        //Imprimimos los resultados por pantalla en inglés y en español
+        System.out.println("La persona con altura " + alturaMetros + " metros " + " y peso de " + pesoGr + " gr tiene un BMR de " + BMR + " calorias");
+        System.out.println("The person whose height is " + alturaFt + " ft " + " and whose weight is " + pesoLb + " Lb has a BMR of " + BMR + " calories");
+        return BMR;
+    }
+
+
+}
