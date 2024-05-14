304206bf61149b3c98f73acce2b9ca2c7e2d8af4
teresavgm <160473130+teresavgm@users.noreply.github.com>
2024-05-14 22:30:18 +0200

    Modifco los metodos de DecoradorEuropeo.java y pongo como parametro un objeto de tipo Paciente

--- src/main/java/healthcalc/DecoradorEuropeo.java
+++ src/main/java/healthcalc/DecoradorEuropeo.java
@@ -9,18 +9,18 @@ public class DecoradorEuropeo extends DecoratorHealthHospital {
     }
 
     @Override
-    public int pesoIdeal(Gender genero,float altura) throws Exception {
-        return super.pesoIdeal(genero, altura);
+    public int pesoIdeal(Paciente pa) throws Exception {
+        return super.pesoIdeal(pa);
     }
 
     @Override
-    public double bmr(Gender genero, int edad, float alturaMetros, int pesoGr) throws Exception {
-        float BMR = (float) super.bmr(genero, edad,alturaMetros, pesoGr );
-        double alturaFt = alturaMetros*3.28084;
-        int pesoLb = (int) (pesoGr * 0.00220462);
+    public double bmr(Paciente pa) throws Exception {
+        float BMR = (float) super.bmr(pa );
+        double alturaFt = pa.height()*3.28084;
+        int pesoLb = (int) (pa.weight() * 0.00220462);
 
         //Imprimimos los resultados por pantalla en inglés y en español
-        System.out.println("La persona con altura " + alturaMetros + " metros " + " y peso de " + pesoGr + " gr tiene un BMR de " + BMR + " calorias");
+        System.out.println("La persona con altura " + pa.height() + " metros " + " y peso de " + pa.weight() + " gr tiene un BMR de " + BMR + " calorias");
         System.out.println("The person whose height is " + alturaFt + " ft " + " and whose weight is " + pesoLb + " Lb has a BMR of " + BMR + " calories");
         return BMR;
     }
