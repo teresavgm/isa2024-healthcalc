7e8638727d5a8fb1ce37f0cbe7c00c2554b30c10
teresavgm <160473130+teresavgm@users.noreply.github.com>
2024-05-14 22:34:55 +0200

    Modifco los metodos de HealthCalcImpl.java y pongo como parametro un objeto de tipo Paciente

--- src/main/java/healthcalc/HealthCalcImpl.java
+++ src/main/java/healthcalc/HealthCalcImpl.java
@@ -17,21 +17,21 @@ public static HealthCalcImpl getInstance(){
 
     
     @Override
-    public float idealWeight(int height, Gender gender) throws Exception {
+    public float idealWeight(Paciente p) throws Exception {
         // calculamos su peso ideal siguiendo la formula de Lorentz:
         //For men: IW = height - 100 - (height - 150) / 4)
 	    //For women: IW = height - 100 - (height - 150) / 2.5)
         float idealW = 0;
 
-        if(height<=0){
+        if(p.height()<=0){
             throw new RuntimeException("Altura introducida no valida: es menor o igual que 0");
         }else{
-            switch (gender) {
+            switch (p.gender()) {
                 case FEMALE:
-                    idealW = height - 100 - (height - 150) / 2.5f;
+                    idealW = p.height() - 100 - (p.height() - 150) / 2.5f;
                     break;
                 case MALE:
-                    idealW = height - 100 - (height - 150) / 4;
+                    idealW = p.height() - 100 - (p.height() - 150) / 4;
                     break;
                 default:
                     throw new RuntimeException("Género introducido no válido");
@@ -40,29 +40,29 @@ public static HealthCalcImpl getInstance(){
         return idealW;
 
 	}
-	
-    public float basalMetabolicRate(float weight, int height, Gender gender, int age) throws Exception {
+    @Override
+    public float basalMetabolicRate(Paciente p) throws Exception {
         // Calculamos la Tasa Metabólica Basal (TMB) de una persona con la siguiente fórmula:
         // Para hombres: BMR = 10 * weight + 6.25 * height - 5 * age + 5
         // Para mujeres: BMR = 10 * weight + 6.25 * height - 5 * age - 161
     
         float BMR = 0;
     
-        if (height <= 0) {
+        if (p.height() <= 0) {
             throw new RuntimeException("Altura introducida no válida: es menor o igual que 0");
-        } else if (gender == null) {
+        } else if (p.gender() == null) {
             throw new RuntimeException("Género introducido no válido");
-        } else if (weight <= 0) {
+        } else if (p.weight() <= 0) {
             throw new RuntimeException("Peso introducido no válido: es menor o igual que 0");
-        } else if (age <= 0) {
+        } else if (p.age() <= 0) {
             throw new RuntimeException("Edad introducida no válida: es menor o igual que 0");
         } else {
-            switch (gender) {
+            switch (p.gender()) {
                 case FEMALE:
-                    BMR = 10f * weight + 6.25f * height - 5f * age - 161f;
+                    BMR = 10f * p.weight() + 6.25f * p.height() - 5f * p.age() - 161f;
                     break;
                 case MALE:
-                    BMR = 10f * weight + 6.25f * height - 5f * age + 5f;
+                    BMR = 10f * p.weight() + 6.25f * p.height() - 5f * p.age() + 5f;
                     break;
                 default:
                     throw new RuntimeException("Género introducido no válido");
